afbf03977a38a2373cdc4cb38609bfc95da4c3d9
Gustavo Mantuan <gustavo.mantuan@gs.liferay.com>
2021-02-01 00:36:20 -0300

    Refactor Context to use it with custom hooks

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,4 +1,5 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
+import cuid from 'cuid'
 
 export const SET_CURRENT_DATE = 'SET_CURRENT_DATE'
 export const ADD_REMINDER = 'ADD_REMINDER'
@@ -12,6 +13,12 @@ export const CLEAR_REMINDERS = 'CLEAR_REMINDERS'
 export type ReminderId = {
   reminderDate: string
   id?: string
+}
+
+type Action = {
+  type: string
+  //eslint-disable-next-line
+  payload: any
 }
 
 export type State = {
@@ -68,6 +75,203 @@ export const INITIAL_STATE = {
   selectReminder: () => undefined,
   clearReminders: () => undefined,
   setCurrentDate: () => undefined
+}
+
+export const useReducerForContext = () => {
+  const [state, dispatch] = useReducer(
+    (prevState: State, action: Action) => {
+      switch (action.type) {
+        case SET_CURRENT_DATE: {
+          const { currentDate } = action.payload
+          return {
+            ...prevState,
+            currentMonth: currentDate.getMonth(),
+            currentDate: currentDate
+          }
+        }
+        case OPEN_MODAL: {
+          return {
+            ...prevState,
+            isModalOpen: true,
+            reminderDate: action.payload.reminderDate
+          }
+        }
+        case CLOSE_MODAL: {
+          return {
+            ...prevState,
+            isModalOpen: false,
+            selectedReminder: undefined
+          }
+        }
+        case SELECT_REMINDER: {
+          return {
+            ...prevState,
+            selectedReminder: {
+              ...action.payload
+            }
+          }
+        }
+        case ADD_REMINDER: {
+          const { reminderDate } = action.payload
+          const id = cuid()
+          return {
+            ...prevState,
+            isModalOpen: false,
+            reminders: {
+              ...prevState.reminders,
+              [reminderDate]: prevState.reminders[reminderDate]
+                ? prevState.reminders[reminderDate].concat([
+                    {
+                      id,
+                      ...action.payload
+                    }
+                  ])
+                : [
+                    {
+                      id,
+                      ...action.payload
+                    }
+                  ]
+            }
+          }
+        }
+        case EDIT_REMINDER: {
+          const { reminderDate, id } = action.payload
+          const remindersLeft = prevState.reminders[reminderDate].filter(
+            (reminder: Reminder) => reminder.id !== id
+          )
+          return {
+            ...prevState,
+            isModalOpen: false,
+            selectedReminder: undefined,
+            reminders: {
+              ...prevState.reminders,
+              [reminderDate]: [...remindersLeft].concat([
+                {
+                  id,
+                  ...action.payload
+                }
+              ])
+            }
+          }
+        }
+        case CLEAR_REMINDERS: {
+          const { reminderDate } = action.payload
+          return {
+            ...prevState,
+            reminders: {
+              ...prevState.reminders,
+              [reminderDate]: []
+            }
+          }
+        }
+        case DELETE_REMINDER: {
+          const { reminderDate, id } = action.payload
+          const remindersLeft = prevState.reminders[reminderDate].filter(
+            (reminder: Reminder) => reminder.id !== id
+          )
+          return {
+            ...prevState,
+            isModalOpen: false,
+            selectedReminder: undefined,
+            reminders: {
+              ...prevState.reminders,
+              [reminderDate]: [...remindersLeft]
+            }
+          }
+        }
+        default:
+          return {
+            ...prevState
+          }
+      }
+    },
+    {
+      ...INITIAL_STATE
+    }
+  )
+  return { state, dispatch }
+}
+
+type ContextProviderType = {
+  state: State
+  dispatch: ({ type, payload }: Action) => void
+}
+
+export const useContextProvider = ({
+  state,
+  dispatch
+}: ContextProviderType) => {
+  return {
+    setCurrentDate: (currentDate: Date) => {
+      dispatch({
+        type: SET_CURRENT_DATE,
+        payload: { currentDate }
+      })
+    },
+    addReminder: ({ reminder, time, color, reminderDate, city }: Reminder) => {
+      dispatch({
+        type: ADD_REMINDER,
+        payload: { reminder, time, color, reminderDate, city }
+      })
+    },
+    editReminder: ({
+      reminder,
+      time,
+      color,
+      reminderDate,
+      city,
+      id
+    }: Reminder) => {
+      dispatch({
+        type: EDIT_REMINDER,
+        payload: { reminder, time, color, reminderDate, city, id }
+      })
+    },
+    openModal: (reminderDate: string) => {
+      dispatch({
+        type: OPEN_MODAL,
+        payload: { reminderDate }
+      })
+    },
+    closeModal: () => {
+      dispatch({
+        type: CLOSE_MODAL,
+        payload: null
+      })
+    },
+    selectReminder: ({
+      reminder,
+      time,
+      color,
+      reminderDate,
+      city,
+      id
+    }: Reminder) => {
+      dispatch({
+        type: SELECT_REMINDER,
+        payload: { reminder, time, color, reminderDate, city, id }
+      })
+    },
+    deleteReminder: ({ reminderDate, id }: ReminderId) => {
+      dispatch({
+        type: DELETE_REMINDER,
+        payload: { reminderDate, id }
+      })
+    },
+    clearReminders: ({ reminderDate }: ReminderId) => {
+      dispatch({
+        type: CLEAR_REMINDERS,
+        payload: { reminderDate }
+      })
+    },
+    currentDate: state.currentDate,
+    currentMonth: state.currentMonth,
+    isModalOpen: state.isModalOpen,
+    reminders: state.reminders,
+    selectedReminder: state.selectedReminder,
+    reminderDate: state.reminderDate
+  }
 }
 
 export const ReminderContext = createContext<State>(INITIAL_STATE)
